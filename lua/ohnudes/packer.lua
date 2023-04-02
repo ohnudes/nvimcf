91f143b61b8e42e149c32f927b0fd8fead4ea53c
Nudes <nivohq@gmail.com>
2023-04-02 22:26:57 +0200

    formatting in packer.lua

--- lua/ohnudes/packer.lua
+++ lua/ohnudes/packer.lua
@@ -17,7 +17,7 @@ return require('packer').startup(function(use)
 
 	vim.cmd('colorscheme rose-pine')
 
-	use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'}) 
+	use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
 	use('nvim-treesitter/playground')
 	use('theprimeagen/harpoon')
 	use('mbbill/undotree')
