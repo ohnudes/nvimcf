5b69286fe82c6563b3858f410d9ebaa5c75c32f8
Nudes <nivohq@gmail.com>
2023-04-04 16:19:15 +0200

    Added: interaction between nvim and OS clipboard.

--- lua/ohnudes/set.lua
+++ lua/ohnudes/set.lua
@@ -1,3 +1,16 @@
+vim.g.clipboard = {
+   name = 'WslClipboard',
+   copy = {
+      ['+'] = 'clip.exe',
+      ['*'] = 'clip.exe',
+   },
+   paste = {
+      ['+'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+      ['*'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+   },
+   cache_enabled = 0,
+}
+
 vim.opt.guicursor = ""
 
 vim.opt.nu = true
